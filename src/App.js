32376785a9698b98c0dd45316f870ac32161c682
pedro558 <pedroafonso109@gmail.com>
2023-06-06 15:19:08 -0300

    :sparkles: feat: Error page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
 import { Products } from './pages/Products';
+import { Error } from './pages/Error';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/products' element={<Products/>}/>
+        <Route path='*' element={<Error/>}/>
       </Routes>
     </Router>
   );
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,12 @@
-const Error = () => {
-  return (
-    <section className='section'>
-      <h2>Error</h2>
+import{ Link } from 'react-router-dom'
+
+export function Error() {
+  return(
+    <section className="section">
+      <h2>404</h2>
+      <p>Page not found</p>
+      <Link to='/' className='btn'>Back to Home</Link>
     </section>
-  );
+  )
 };
-export default Error;
+
