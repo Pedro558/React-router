f7f4c9d8ce74c7832c344357dc595a75ab9ef9db
pedro558 <pedroafonso109@gmail.com>
2023-06-06 15:04:26 -0300

    :sparkles: add Link to About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export function About(){
   return(
     <section className='section'>
       <h2>About</h2>
+
+      <Link to='/' className="btn">Back to Home</Link>
     </section>
   )
 }
