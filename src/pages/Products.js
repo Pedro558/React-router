0a5f3e40e1e7a0d7269e334c576678a309bf1fd7
pedro558 <pedroafonso109@gmail.com>
2023-06-07 13:55:57 -0300

    :sparkles: feat: grabbing products from data.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
+import products from "../data";
 
 export  function Products() {
   return(
-    <>
-      <section className='section'>
-        <h2>products</h2>
-        <Link to='/' className="btn">Back to Home</Link>
-      </section>
-    </>
+    <section className='section'>
+      <h2>Products</h2>
+      <div className="products">
+        {products.map(product => {
+          return(
+            <article key={product.id}>
+              <h5>{product.name}</h5>
+              <Link to={`${product.id}`}>more info</Link>
+            </article>
+          )
+        })}
+      </div>
+      <Link to='/' className="btn">Back to Home</Link>
+    </section>
   )
 }
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,13 +1,16 @@
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import products from '../data'
 
 export function SingleProduct() {
-
   const { productId } = useParams()
+  const product = products.find(product => product.id === productId)
+  const { image, name } = product
 
   return(
     <section className='section product'>
-      <h2>single product</h2>
-      <p>Id = {productId}</p>
+      <img src={image} alt={name}/>
+      <h5>{name}</h5>
+      <Link to={'/products'}>Back to products</Link>
     </section>
   )
   
