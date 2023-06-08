a3ee940860b13e6d093efd5c1e5d6888c04c44e8
pedro558 <pedroafonso109@gmail.com>
2023-06-08 11:03:51 -0300

    :recycle: refactor: Product route in App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Error } from './pages/Error';
 import { SharedLayout } from './pages/SharedLayout';
 import { SingleProduct } from './pages/SingleProduct';
 import { ProtectedRoute } from './pages/ProtectedRoute';
+import { SharedProductLayout } from './pages/SharedProductLayout';
 
 import  Dashboard  from './pages/Dashboard'
 import Login from './pages/Login'
@@ -20,13 +21,18 @@ export default function App() {
         <Route path='/' element={<SharedLayout/>}>
           <Route index element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
-          <Route path='/products' element={<Products/>}/>
-          <Route path='/products/:productId' element={<SingleProduct/>}/>
+
+          <Route path='/products' element={<SharedProductLayout/>}>
+            <Route index element={<Products/>}/>
+            <Route path=':productId' element={<SingleProduct/>}/>
+          </Route>
+
           <Route path='/dashboard' element={
             <ProtectedRoute user={user}>
               <Dashboard user={user}/>
             </ProtectedRoute>
           }/>
+
           <Route path='/login' element={<Login setUser={setUser}/>}/>
         </Route>
 
@@ -35,4 +41,3 @@ export default function App() {
     </Router>
   );
 }
-
